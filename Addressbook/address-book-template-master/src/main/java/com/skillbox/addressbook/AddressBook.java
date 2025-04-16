45cf72dcfff4db7fa226d73ab0803e93e07a7616
LinnikA <a@linnik-1.ru>
2025-04-16 10:41:43 +0300

    changed equals in Contact

--- Addressbook/address-book-template-master/src/main/java/com/skillbox/addressbook/AddressBook.java
+++ Addressbook/address-book-template-master/src/main/java/com/skillbox/addressbook/AddressBook.java
@@ -4,28 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBook {
-private List<Contact> contacts;
-public AddressBook(){contacts=new ArrayList<Contact>();}
-public void  addContact(Contact contact){
-if(contacts.isEmpty()) {
-    System.out.println("В книге контактов нет записей");
-    contacts.add(contact);
-} else {
+    private List<Contact> contacts;
 
-    for (Contact c:contacts){
-        if(c.getSurname().equals(contact.getSurname())&& c.getName().equals(contact.getName())){
-            System.out.println("Такой контакт существует и будет перезаписан");
-            contacts.set(contacts.indexOf(c), contact);
-            return;
+    public AddressBook() {
+        contacts = new ArrayList<Contact>();
+    }
+
+    private void addContact(Contact contact) {
+        if (contacts.isEmpty()) {
+            System.out.println("В книге контактов нет записей");
+            contacts.add(contact);
+        } else {
+
+            for (Contact c : contacts) {
+                if (c.getSurname().equals(contact.getSurname()) && c.getName().equals(contact.getName())) {
+                    System.out.println("Такой контакт существует и будет перезаписан!");
+                    contacts.set(contacts.indexOf(c), contact);
+                    return;
+                } else contacts.add(contact);
+            }
+
+
         }
-        else contacts.add(contact);
-}
 
 
     }
 
+    private void getContact(Contact contact, String surname) {
+        for (Contact с : contacts) {
+            if (с.getSurname().equals(surname)) {
+                System.out.println(с);
+                break;
+            }
+        }
 
-}
+    }
+
+
+    private void deleteContactBySurname(Contact contact, String surname) {
+        Contact contactToRemove = null;
+        for (Contact с : contacts) {
+            if (с.getSurname().equals(surname)) {
+                contactToRemove = с;
+                break;
+            }
+        }
+        if (contactToRemove != null) {
+            contacts.remove(contactToRemove);
+
+        }
+    }
     // Здесь будут храниться контакты.
     // Вам надо придумать, каким образом эти контакты будут храниться
     // в адресной книге.
--- Addressbook/address-book-template-master/src/main/java/com/skillbox/addressbook/Contact.java
+++ Addressbook/address-book-template-master/src/main/java/com/skillbox/addressbook/Contact.java
@@ -1,33 +1,33 @@
 package main.java.com.skillbox.addressbook;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
-import java.util.List;
 import java.util.Objects;
-import java.util.UUID;
 
 @Getter
 @Setter
 
 
-public class Contact  {
-    private  int id=1;
-    private int nextId=1;
-    private  String name;
-    private  String surname;
+public class Contact {
+    private int id = 1;
+    private int nextId = 1;
+    private String name;
+    private String surname;
     private Date birthday;
     private String email;
-    private  String phoneNumber;
+    private String phoneNumber;
     private String post;
 
     public Contact(String name, String surname, Date birthday, String email, String phoneNumber, String post) {
-        this.name = name;
-        this.surname = surname;
+        this.name = name.toLowerCase();
+        this.surname = surname.toLowerCase();
         this.birthday = birthday;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.post = post;
-        id=nextId;
+        id = nextId;
         nextId++;
     }
 
@@ -35,7 +35,7 @@ public class Contact  {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Contact contact = (Contact) o;
-        return this.name== contact.name && this.surname== contact.surname;
+        return Objects.equals(name, contact.name) && Objects.equals(surname, contact.surname);
     }
 
     @Override
@@ -45,15 +45,15 @@ public class Contact  {
 
     @Override
     public String toString() {
-        return "Имя:"+name +"\nФамилия:"+surname +"\nДата рождения:"+birthday+"\nemail:"+email+
-                "\nТелефон:"+phoneNumber+ "\nДолжность:"+post;
+        return "Имя:" + name + "\nФамилия:" + surname + "\nДата рождения:" + birthday + "\nemail:" + email +
+                "\nТелефон:" + phoneNumber + "\nДолжность:" + post;
     }
 
 }
 
 
 // Опишите тут необходимые поля и методы доступа к этим полям.
-    // Переопределите конструктор по умолчанию, методы hashCode() и equals()
-    // Будет удобно, если вы также переопределите метод toString() для
-    // вывода информации о контакте
+// Переопределите конструктор по умолчанию, методы hashCode() и equals()
+// Будет удобно, если вы также переопределите метод toString() для
+// вывода информации о контакте
 
