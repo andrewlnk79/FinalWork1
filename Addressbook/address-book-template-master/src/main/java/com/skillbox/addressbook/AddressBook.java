4db6df1e77777930f7bbb7bf8148a9e5ba5f03c1
LinnikA <a@linnik-1.ru>
2025-04-16 09:58:34 +0300

    added addContact()

--- Addressbook/address-book-template-master/src/main/java/com/skillbox/addressbook/AddressBook.java
+++ Addressbook/address-book-template-master/src/main/java/com/skillbox/addressbook/AddressBook.java
@@ -1,6 +1,31 @@
 package main.java.com.skillbox.addressbook;
-public class AddressBook {
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+private List<Contact> contacts;
+public AddressBook(){contacts=new ArrayList<Contact>();}
+public void  addContact(Contact contact){
+if(contacts.isEmpty()) {
+    System.out.println("В книге контактов нет записей");
+    contacts.add(contact);
+} else {
+
+    for (Contact c:contacts){
+        if(c.getSurname().equals(contact.getSurname())&& c.getName().equals(contact.getName())){
+            System.out.println("Такой контакт существует и будет перезаписан");
+            contacts.set(contacts.indexOf(c), contact);
+            return;
+        }
+        else contacts.add(contact);
+}
+
+
+    }
+
+
+}
     // Здесь будут храниться контакты.
     // Вам надо придумать, каким образом эти контакты будут храниться
     // в адресной книге.
--- Addressbook/address-book-template-master/src/main/java/com/skillbox/addressbook/Contact.java
+++ Addressbook/address-book-template-master/src/main/java/com/skillbox/addressbook/Contact.java
@@ -1,8 +1,59 @@
 package main.java.com.skillbox.addressbook;
 
-public class Contact {
-    // Опишите тут необходимые поля и методы доступа к этим полям.
-    // Переопределите конструктор по умолчанию, методы hashCode() и equals()
-    // Будет удобно, если вы также переопределитеметод toString() для
-    // вывода информации о контакте
+import lombok.*;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+
+
+public class Contact  {
+    private  int id=1;
+    private int nextId=1;
+    private  String name;
+    private  String surname;
+    private Date birthday;
+    private String email;
+    private  String phoneNumber;
+    private String post;
+
+    public Contact(String name, String surname, Date birthday, String email, String phoneNumber, String post) {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.post = post;
+        id=nextId;
+        nextId++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return this.name== contact.name && this.surname== contact.surname;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    @Override
+    public String toString() {
+        return "Имя:"+name +"\nФамилия:"+surname +"\nДата рождения:"+birthday+"\nemail:"+email+
+                "\nТелефон:"+phoneNumber+ "\nДолжность:"+post;
+    }
+
 }
+
+
+// Опишите тут необходимые поля и методы доступа к этим полям.
+    // Переопределите конструктор по умолчанию, методы hashCode() и equals()
+    // Будет удобно, если вы также переопределите метод toString() для
+    // вывода информации о контакте
+
